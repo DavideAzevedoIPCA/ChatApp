aa39f6540fb506ec07992e04b81c8f6f3c2197c3
Miguel Fernandes <a12556@alunos.ipca.pt>
2022-05-25 22:49:46 +0100

    Create fragment using in home activity

--- app/src/main/java/com/example/chatapp/HomeActivity.kt
+++ app/src/main/java/com/example/chatapp/HomeActivity.kt
@@ -2,10 +2,14 @@ package com.example.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
     }
+
+    fun launchFragment(view: View) {}
+    fun launchFragmentUser(view: View) {}
 }

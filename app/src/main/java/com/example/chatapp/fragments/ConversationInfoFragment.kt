d8b5257043aa1a7dbaa8035fec3387ef26eacae1
Artur Pereira <arturpereira91997@gmail.com>
2022-06-22 22:40:00 +0100

    Updated conv info fragment

--- app/src/main/java/com/example/chatapp/fragments/ConversationInfoFragment.kt
+++ app/src/main/java/com/example/chatapp/fragments/ConversationInfoFragment.kt
@@ -3,21 +3,21 @@ package com.example.chatapp.fragments
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chatapp.ConversationActivity
 import com.example.chatapp.R
 import com.example.chatapp.adapters.ConvInfoAdapter
+import com.example.chatapp.models.Conversation
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 private const val ARG_PARAM1 = "param1"
@@ -26,6 +26,11 @@ private const val ARG_PARAM2 = "param2"
 class ConversationInfoFragment : Fragment() {
     private var name: String? = null
     private var img: String? = null
+
+    val db = FirebaseFirestore.getInstance()
+
+    //TODO RECEIVE CONVERSATION UID FROM ACTIVITY
+    val uid = "dwee2312313"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +50,7 @@ class ConversationInfoFragment : Fragment() {
         val plus_btn = view.findViewById<ImageButton>(R.id.fragConvInf_plus_bt)
 
         val add_et = view.findViewById<EditText>(R.id.fragConvInf_add_et)
+        val add_bt = view.findViewById<Button>(R.id.fragConvInf_add_bt)
 
         back_btn.setOnClickListener {
 
@@ -54,30 +60,32 @@ class ConversationInfoFragment : Fragment() {
             if( add_et.visibility == View.GONE )
             {
                 add_et.visibility = View.VISIBLE
+                add_bt.visibility= View.VISIBLE
             }
             else if ( add_et.visibility == View.VISIBLE )
             {
                 add_et.visibility = View.GONE
+                add_bt.visibility = View.GONE
             }
         }
 
-        add_et.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
-            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+        add_bt.setOnClickListener {
+            val users = db.collection("users")
+            val conversations = db.collection("conversations")
+            val new_member = add_et.text.toString()
 
-                /*
-                    if ( add_et.text == img ) {
-                        add_et.visibility = View.GONE
-                        Toast.makeText(this,getString(R.string.fragConvInfo_user_added), Toast.LENGTH_LONG).show()
-                    }
-                        else  {
-                        Toast.makeText(this,getString(R.string.fragConvInfo_user_not_found), Toast.LENGTH_LONG).show()
-                    }
-                */
+            val query = users.whereEqualTo("email", new_member)
 
-                return@OnKeyListener true
-            }
-            false
-        })
+            Log.d("Query Result", query.toString())
+
+                if (query.toString() != null) {
+                    add_et.visibility = View.GONE
+                    add_bt.visibility = View.GONE
+                    Toast.makeText(activity, getString(R.string.fragConvInfo_user_added), Toast.LENGTH_LONG).show()
+                    //TODO ALTERAR LINHA ABAIXO PARA RECEBER UID CORRETAMENTE E ALTERAR CORRETAMENTE OS UTILIZADORES
+                    conversations.document(uid).update("users", query)
+                }
+        }
 
         view.findViewById<RecyclerView>(R.id.fragConvInf_members_rv).layoutManager = LinearLayoutManager(activity)
         view.findViewById<RecyclerView>(R.id.fragConvInf_members_rv).adapter= ConvInfoAdapter((listOf("A","B","C","D","E","F","G","H","I","J")))

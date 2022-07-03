b3b91ba1ed708994e9c4768e3e601b93ed3dc486
Artur Pereira <arturpereira91997@gmail.com>
2022-07-03 03:18:13 +0100

    ConvInfoFrag devs
    populating RecyclerView and maping data into Fragment

--- app/src/main/java/com/example/chatapp/adapters/ConvInfoAdapter.kt
+++ app/src/main/java/com/example/chatapp/adapters/ConvInfoAdapter.kt
@@ -3,21 +3,24 @@ package com.example.chatapp.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.chatapp.models.User
 
-class ConvInfoAdapter(private val mList: List<String>) : RecyclerView.Adapter<ConvInfoAdapterViewHolder>() {
+class ConvInfoAdapter(private val mList: MutableList<User>) : RecyclerView.Adapter<ConvInfoAdapterViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ConvInfoAdapterViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         return ConvInfoAdapterViewHolder(inflater, parent)
     }
     override fun onBindViewHolder(holder: ConvInfoAdapterViewHolder, position: Int) {
-        val color = when(position % 2) {
-            0 -> android.R.color.holo_red_dark
-            1 -> android.R.color.holo_blue_dark
-            else -> android.R.color.holo_orange_dark
-        }
-        val text = mList.get(position) ?: "????"
-        holder.bindData(text,color)
+        val user = mList.get(position)
+        holder.bindData(user)
     }
+
+    fun refreshDate(list : List<User>) {
+        this.mList.clear()
+        this.mList.addAll(list)
+        notifyDataSetChanged()
+    }
+
     override fun getItemCount(): Int {
         return mList.size
     }
--- app/src/main/java/com/example/chatapp/fragments/ConversationInfoFragment.kt
+++ app/src/main/java/com/example/chatapp/fragments/ConversationInfoFragment.kt
@@ -1,23 +1,21 @@
 package com.example.chatapp.fragments
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.KeyEvent
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.chatapp.ConversationActivity
 import com.example.chatapp.R
 import com.example.chatapp.adapters.ConvInfoAdapter
 import com.example.chatapp.models.Conversation
+import com.example.chatapp.models.User
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 private const val ARG_PARAM1 = "param1"
@@ -27,34 +25,44 @@ class ConversationInfoFragment : Fragment() {
     private lateinit var conversation : Conversation
     private var name: String? = null
     private var img: String? = null
+    private var listUsers: MutableList<User> = mutableListOf()
 
     val db = FirebaseFirestore.getInstance()
-
-    //TODO RECEIVE CONVERSATION UID FROM ACTIVITY
-    val uid = "dwee2312313"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-             //name = it.getString(ARG_PARAM1)
-             //img = it.getString(ARG_PARAM2)
+            //name = it.getString(ARG_PARAM1)
+            //img = it.getString(ARG_PARAM2)
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?
-                            ): View? {
+    ): View? {
+
         val view = inflater.inflate(R.layout.fragment_conversation_info, container, false)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.fragConvInf_members_rv)
+
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter= ConvInfoAdapter(listUsers)
 
         val back_btn = view.findViewById<ImageButton>(R.id.fragconvInf_back_bt)
 
         val plus_btn = view.findViewById<ImageButton>(R.id.fragConvInf_plus_bt)
 
+        val ConvInfoTitle = view.findViewById<TextView>(R.id.fragConvInf_title_tv)
         val add_et = view.findViewById<EditText>(R.id.fragConvInf_add_et)
         val add_bt = view.findViewById<Button>(R.id.fragConvInf_add_bt)
 
+        ConvInfoTitle.text = conversation.title
+
+
         back_btn.setOnClickListener {
 
+            activity?.onBackPressed();
+            // needs bugfixing
         }
 
         plus_btn.setOnClickListener {
@@ -71,25 +79,40 @@ class ConversationInfoFragment : Fragment() {
         }
 
         add_bt.setOnClickListener {
-            val users = db.collection("users")
-            val conversations = db.collection("conversations")
+
             val new_member = add_et.text.toString()
+            db.collection("users")
+                .whereEqualTo("email", new_member)
+                .get().addOnSuccessListener { values ->
+                    var user : User = User()
+                    values?.forEach{
+                        user = User()
 
-            val query = users.whereEqualTo("email", new_member)
+                        user.uid = it.data["uid"].toString()
+                        user.name = it.data["name"].toString()
+                        user.email = it.data["email"].toString()
+                        user.photo_url = it.data["photo_url"].toString()
 
-            Log.d("Query Result", query.toString())
+                        GlobalScope.launch {
+                            listUsers.add(user)
+                        }
+                            if(recyclerView.adapter is ConvInfoAdapter){
+                                (recyclerView.adapter as ConvInfoAdapter).refreshDate(listUsers)
+                            }
 
-                if (query.toString() != null) {
-                    add_et.visibility = View.GONE
-                    add_bt.visibility = View.GONE
-                    Toast.makeText(activity, getString(R.string.fragConvInfo_user_added), Toast.LENGTH_LONG).show()
-                    //TODO ALTERAR LINHA ABAIXO PARA RECEBER UID CORRETAMENTE E ALTERAR CORRETAMENTE OS UTILIZADORES
-                    conversations.document(uid).update("users", query)
+                    }
                 }
+
+            Log.d("New Member", new_member)
+            Log.d("Conversation info", conversation.toString())
+            Log.d("Conversation id", conversation.id)
+            Log.d("Conversation title", conversation.title)
+            Log.d("Conversation users", conversation.users.toString())
+            Log.d("Conversation LastMessag", conversation.lastMessage.toString())
+
         }
 
-        view.findViewById<RecyclerView>(R.id.fragConvInf_members_rv).layoutManager = LinearLayoutManager(activity)
-        view.findViewById<RecyclerView>(R.id.fragConvInf_members_rv).adapter= ConvInfoAdapter((listOf("A","B","C","D","E","F","G","H","I","J")))
+
 
         return view
     }
